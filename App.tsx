1c96a255720395aa793b1a71338caec97c003733
baonguyen <bao17030202@gmail.com>
2024-01-08 15:12:41 +0700

    +css

--- App.tsx
+++ App.tsx
@@ -14,11 +14,10 @@ const App = () => {
             <PaperProvider>
                 <NavigationContainer>
                     <Stack.Navigator>
-                        <Stack.Screen name="Audio" component={AudioScreen} />
-
                         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
                         <Stack.Screen name="Home" component={HomeScreen} />
                         <Stack.Screen name="Details" component={DetailsScreen} />
+                        <Stack.Screen name="Audio" component={AudioScreen} />
                     </Stack.Navigator>
                 </NavigationContainer>
             </PaperProvider>
