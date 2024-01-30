bc3367f220ea45d412c9d131a340b0fff9c032e9
Nguyen Ngoc Tien <64886876+tientulac@users.noreply.github.com>
2024-01-30 11:37:36 +0700

    c

--- App.tsx
+++ App.tsx
@@ -4,14 +4,32 @@ import store from './src/redux-store/store';
 import AppNavigationContainer from './src/navigation';
 import { useEffect } from 'react';
 import { createSignalRConnection, startConnection } from './src/common/signalR';
+import Toast from 'react-native-toast-message';
+import { Message } from './src/model/message';
+import { Ultility } from './src/common/ultility';
 
 const App = () => {
 
     useEffect(() => {
         const connection = createSignalRConnection();
         startConnection(connection);
-        connection.on("ReceiveNotification", (userId, name, status) => {
-            console.log('Received Notification: ', userId, name, status);
+        connection.on("ReceiveNotification", (message: Message) => {
+            if (Ultility.getUserInfo().id == message.userAccountId) {
+                Toast.show({
+                    type: 'error',
+                    text1: 'Thông báo duyệt',
+                    text2: `${message.type == 0 ? `Bài đăng "${message.name}" của bạn ${message.status}` : message.type == 1 ? `Bình luận của bạn ${message.status}` : ''} 👋`
+                });
+            }
+            else {
+                if (message.type == 0) {
+                    Toast.show({
+                        type: 'error',
+                        text1: 'Thông báo duyệt',
+                        text2: `Bài đăng "${message.name}" ${message.status} 👋`
+                    });
+                }
+            }
         });
         return () => {
             connection.stop();
@@ -22,6 +40,7 @@ const App = () => {
         <Provider store={store}>
             <PaperProvider>
                 <AppNavigationContainer></AppNavigationContainer>
+                <Toast />
             </PaperProvider>
         </Provider>
     );
